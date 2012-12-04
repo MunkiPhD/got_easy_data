398bc2cbf2394bc7dac894969651da1bc883d5b7
MunkiPhD <munkiphd@gmail.com>
2012-12-03 21:28:10 -0500

    - Initial commit - removed public/index - added home controller - added swp files to .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GotEasyData::Application.config.secret_token = '488021e91760767c441c1d6afaaead15232c59e14d8ca2b54ef2e29e66b4fde2684ee85c77d2032e236dde48189e210d749b147c64e0244f8c18448fca43591f'
