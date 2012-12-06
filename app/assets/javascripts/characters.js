d7d568a7b833581c16383feffb3e294da146ebf8
MunkiPhD <munkiphd@gmail.com>
2012-12-05 21:43:57 -0500

    Added javascript for ease of use

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -23,8 +23,21 @@ $(document).ready(function(){
 			alert(e);
 		}
 	});
+
+	// When we show the modal window to add a house, focus on the first input.
+	// Then when we close it, focus on the house select box.
+	$('#add_house_modal').on('shown', function(){
+		FocusFormsFirstInput('new_house');
+	}).on('hidden', function(){
+		$('#character_house_id').focus();
+	});
 });
 
+
+// Fill the specified select box with the data provided
+//    id: the ID of the select (don't include the jquery # selector
+//    data: The JSON data that will be used to populate the options
+//    defaultOption: the default option for the select (e.g. "Select an option...")
 FillSelect = function(id, data, defaultOption){
 	var selectCtrl = $('#' + id); //character_house_id');
 	$(selectCtrl).empty();
@@ -38,4 +51,13 @@ FillSelect = function(id, data, defaultOption){
 
 		return newOptions;
 	});
+};
+
+
+// Focuses on the first input of the specified container with the ID
+//    id: ID of the container in the html (e.g. "character_id" NOT with the jquery # selector)
+FocusFormsFirstInput = function(id){
+	var formID  = $("#" + id);
+	var firstInput = $(":input:not(input[type=button],input[type=submit],button):visible:first", formID);
+	firstInput.focus();
 };
